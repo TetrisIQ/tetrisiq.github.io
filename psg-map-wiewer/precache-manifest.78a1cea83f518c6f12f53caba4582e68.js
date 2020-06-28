b50db22e0ab7cbb2d9c1504e66b36d2172eb494c
alex <tetrisiq@tuta.io>
2020-06-28 17:58:54 +0200

    added psg map viewer dist folder

---
+++ psg-map-wiewer/precache-manifest.78a1cea83f518c6f12f53caba4582e68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9ce5f81b9ec8bc14dd",
+    "url": "/css/app.efb1765e.css"
+  },
+  {
+    "revision": "a33d50096f11200948ec",
+    "url": "/css/chunk-vendors.954f8d1b.css"
+  },
+  {
+    "revision": "7a07f8f19562b7afeb0ac3fa51e3c3c5",
+    "url": "/img/empty_sky.7a07f8f1.svg"
+  },
+  {
+    "revision": "6a408de589f6067de5e0459cf365b16a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de9ce5f81b9ec8bc14dd",
+    "url": "/js/app.24c3e4ef.js"
+  },
+  {
+    "revision": "a33d50096f11200948ec",
+    "url": "/js/chunk-vendors.2ae29e94.js"
+  },
+  {
+    "revision": "5f3ffa51a9b7418b3b7477a6de90a7ce",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
